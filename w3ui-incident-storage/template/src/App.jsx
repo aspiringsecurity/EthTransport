4d4d6999878bf405f48b24992880fa0f28d5e251
d.kotwal <deeptikotwal@chevron.com>
2023-01-16 16:17:59 +0530

    w3ui for incident storage, single and multiple file uploads after authentication and authorization

---
+++ w3ui-incident-storage/template/src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.png'
+
+function App () {
+  return (
+    <div>
+      <header style={{ textAlign: 'center', padding: 10 }}>
+        <img src={logo} width='250' alt='logo' />
+      </header>
+    </div>
+  )
+}
+
+export default App
